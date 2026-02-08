159d698d4ee52991aef9c4372750ccbbc02039e2
ed0ma <ethandoma@gmail.com>
2026-02-07 19:57:52 -0500

    Implimented chip8 struct and chip8_resest() function

---
+++ src/chip8.h
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+#define MEM_SIZE 4096
+#define DISP_WIDTH 64
+#define DISP_HEIGHT 32
+#define FONT_ADDRESS 0x0
+
+typedef struct Chip8 {
+// Add in chip8 struct
+    uint8_t memory[MEM_SIZE]; //RAM
+    uint8_t registers[16]; //16 8 bit regs
+    uint8_t delay_reg8; //8 bit delay timer
+    uint8_t sound_timer; //8 bit sound timer
+    uint16_t pc; //program counter
+    uint8_t sp; //stack pointer
+    uint16_t I; // 16 bit index reg
+    uint16_t stack[16]; //stack
+    uint16_t keypad[16]; //16 bit keypad state
+    uint8_t display[DISP_HEIGHT][DISP_WIDTH]; //display buffer
+} Chip8;
